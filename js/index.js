d9dec019baddc81013da413a132e0bd228b1fb86
Moaaz-i <famile.sherif@gmail.com>
2025-07-06 13:11:50 +0300

    feat/responsive

--- js/index.js
+++ js/index.js
@@ -1,19 +1,35 @@
-const header = document.getElementById('header');
+const header = document.getElementById("header");
 
 let lastScrollY = window.scrollY;
 
-window.addEventListener('scroll', () => {
-    const currentScrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+  const currentScrollY = window.scrollY;
 
-    if (currentScrollY < lastScrollY || currentScrollY === 0) {
-        header.style.top = '0';
-        if (currentScrollY === 0) {
-            header.style.backgroundColor = "transparent"
-        }
-    } else {
-        header.style.backgroundColor = "#333333"
-        header.style.top = '-100%';
+  if (currentScrollY < lastScrollY || currentScrollY === 0) {
+    header.style.top = "0";
+    header.style.backgroundColor = "#333333";
+    const links = header.getElementsByClassName("links");
+    links.style.width = "100%";
+    if (currentScrollY === 0) {
+      header.style.backgroundColor = "transparent";
     }
+  } else {
+    header.style.top = "-100%";
+  }
 
-    lastScrollY = currentScrollY;
+  lastScrollY = currentScrollY;
+});
+
+const parent_menu_hamburger = document.getElementById("btn");
+
+parent_menu_hamburger.addEventListener("click", () => {
+  const hamburger = document.getElementById("hamburger");
+
+  if (hamburger.style.display === "none") {
+    hamburger.style.display = "block";
+    console.log(hamburger.style.display);
+  } else {
+    hamburger.style.display = "none";
+    console.log(hamburger.style.display);
+  }
 });
